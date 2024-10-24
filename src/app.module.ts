0c8d95a8c0675fe92e2118c469e7f9b0ca4f8824
Caio Flôres <caio.florees2005@gmail.com>
2024-10-24 11:56:58 -0300

    Criando as resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
